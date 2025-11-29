5bca8fbfb9549b63ace7d223bbe028e778da3635
MakoQn <borisovka.club@gmail.com>
2025-11-30 01:58:50 +0300

    update database connection

--- src/utils/db_connection.py
+++ src/utils/db_connection.py
@@ -1,17 +1,34 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from utils.config import Config
 
-DATABASE_URL = "postgresql+psycopg2://makoqn:12345@localhost:5432/airbridge_db"
+config = Config()
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    config.DATABASE_URL,
+    echo=True,
+    pool_pre_ping=True,
+    pool_size=20,
+    max_overflow=30
+)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
+
     try:
         yield db
     finally:
         db.close()
+
+def init_db():
+    from src.models import 
+    
+    Base.metadata.create_all(bind=engine)
